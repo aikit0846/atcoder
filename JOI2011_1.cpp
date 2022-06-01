bee8a6f9ac6ea51ca8dbdd04b3b00e2b906f4dff
taikisuzuki <84704522+taiki-suzuki-1019@users.noreply.github.com>
2022-06-01 21:32:00 +0900

    20220601

--- JOI2011_1.cpp
+++ JOI2011_1.cpp
@@ -13,20 +13,17 @@ int main()
     for (i = 0; i < M; i++) {
         std::cin >> chikei[i];
         for (j = 0; j < N; j++) {
-            Jaccum[i + 1][j + 1] = Jaccum[i + 1][j] + Jaccum[i][j + 1] - Jaccum[i][j];
-            Oaccum[i + 1][j + 1] = Oaccum[i + 1][j] + Oaccum[i][j + 1] - Oaccum[i][j];
-            Iaccum[i + 1][j + 1] = Iaccum[i + 1][j] + Iaccum[i][j + 1] - Iaccum[i][j];
+            Jaccum[i + 1][j + 1] = Jaccum[i + 1][j] + (chikei[i][j] == 'J');
+            Oaccum[i + 1][j + 1] = Oaccum[i + 1][j] + (chikei[i][j] == 'O');
+            Iaccum[i + 1][j + 1] = Iaccum[i + 1][j] + (chikei[i][j] == 'I');
+        }
+    }
 
-            if (chikei[i][j] == 'J') {
-                Jaccum[i + 1][j + 1]++;
-            } else if (chikei[i][j] == 'O') {
-                Oaccum[i + 1][j + 1]++;
-            } else {
-                Iaccum[i + 1][j + 1]++;
-            }
-
-            // std::cout << i + 1 << " " << j + 1 << std::endl;
-            // std::cout << Jaccum[i + 1][j + 1] << " " << Oaccum[i + 1][j + 1] << " " << Iaccum[i + 1][j + 1] <<std::endl;
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            Jaccum[i + 1][j + 1] += Jaccum[i][j + 1];
+            Oaccum[i + 1][j + 1] += Oaccum[i][j + 1];
+            Iaccum[i + 1][j + 1] += Iaccum[i][j + 1];
         }
     }
 
